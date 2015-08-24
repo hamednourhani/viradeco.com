973f81357079ac0f72238841b497d5e3d1ed5e6f
Hamed Nourhani <info@itstar.ir>
2015-08-24 11:43:26 +0430

    38

--- functions.php
+++ functions.php
@@ -935,3 +935,11 @@ class Menu_With_Image extends Walker_Nav_Menu {
     $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
   }
 }
+
+function viradeco_filter_search($query) {
+    if ($query->is_search) {
+  $query->set('post_type', array('post', 'product','project'));
+    };
+    return $query;
+};
+add_filter('pre_get_posts', 'viradeco_filter_search');
